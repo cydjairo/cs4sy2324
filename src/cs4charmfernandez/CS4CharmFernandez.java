5e5c2a5b9a936cc165f0dd92d67e46cc7bb8ad4c
cydjairo <112676628+cydjairo@users.noreply.github.com>
2023-08-31 22:48:56 +0800

    Update CS4CharmFernandez.java
    Finished Exercise 1! :DD

--- src/cs4charmfernandez/CS4CharmFernandez.java
+++ src/cs4charmfernandez/CS4CharmFernandez.java
@@ -10,16 +10,33 @@ package cs4charmfernandez;
  * @author CHARM
  */
 public class CS4CharmFernandez {
-
+    String fname1 = "Mark";
+    String fname2 = "Joshua";
+    String lname = "Tarcelo";
+    String subjfname1 = "Computer";
+    String  subjfname2 = "Social";
+    String subjlname = "Science";
+    int years1 = 7;
+    int years2 = 4;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) 
     {
-        System.out.println("Teacher 1");
-        String name;
-        System.out.println("Name: + name");
-        
+        CS4CharmFernandez myExer = new CS4CharmFernandez();
+        System.out.println("Teacher 1:");
+        System.out.println("Name: " + myExer.fname1 + " " +  myExer.lname);
+        System.out.println("Subject: " + myExer.subjfname1 + " " + myExer.subjlname);
+        System.out.println("Years of Teaching:" + myExer.years1);
+        System.out.println(" ");
+        System.out.println("Teacher 2:");
+        System.out.println("Name: " + myExer.fname2 + " " + myExer.lname);
+        System.out.println("Subject: " + myExer.subjfname2 + " " + myExer.subjlname);
+        System.out.println("Years of Teaching: " + myExer.years2);
+        System.out.println(" ");
+        System.out.println("Total Years of Teaching: " + (myExer.years1 + myExer.years2));
+        System.out.println("Joshua Tarcelo has more years of teaching: " + (myExer.years1 == myExer.years2));
+        System.out.println("The teachers have the same subject: " + (myExer.subjfname1 == myExer.subjfname2));
+        } 
     }
     
-}
