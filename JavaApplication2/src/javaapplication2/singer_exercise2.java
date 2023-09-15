17c8cd7e2ad61c5d70e07065b2c494885889b276
cydjairo <112676628+cydjairo@users.noreply.github.com>
2023-09-15 22:42:14 +0800

    Update singer_exercise2.java

--- JavaApplication2/src/javaapplication2/singer_exercise2.java
+++ JavaApplication2/src/javaapplication2/singer_exercise2.java
@@ -9,38 +9,50 @@ package javaapplication2;
  * @author cyd jairo
  */
 public class singer_exercise2 {
-    String name, favoriteSong;
-    int NoOfPerformances;
-    static int totalPerformances;
-    double earnings;
-    
-    public singer_exercise2 (String n, int num, double earn, String s){
-        name = n;
-        NoOfPerformances = num;
-        earnings = earn;
-        favoriteSong = s;
+    // instance fields
+    private String name;
+    private int noOfPerformances;
+    private static int totalPerformances;
+    private double earnings;
+    private singer_exercise2 favoriteSong;
+
+    // constructor method
+    public singer_exercise2(String name, int noOfPerformances, 
+            double earnings, singer_exercise2 favoriteSong) {
+        this.name = name;
+        this.noOfPerformances = noOfPerformances;
+        this.earnings = earnings;
+        this.favoriteSong = favoriteSong;
+        totalPerformances += noOfPerformances;
     }
-    
-    public void performForTheAudience(int numOfPeople){
-    System.out.println("===");
-    System.out.println("Before:");
-    System.out.println("Performances: " + NoOfPerformances);
-    System.out.println("Earnings: " + earnings);
-    NoOfPerformances = NoOfPerformances + 1;
-    earnings =earnings + 100*numOfPeople;
-    System.out.println(" ");
-    System.out.println("After: ");
-    System.out.println("Performances: " + NoOfPerformances);
-    System.out.println("Earnings: " + earnings);
-    System.out.println("    ");
+
+    public void performForTheAudience(int people) {
+        noOfPerformances++;
+        totalPerformances++;
+        earnings += people * 100;
     }
-    
-    public void changeFavoriteSong(String newSong){
-    System.out.println("==============");
-    System.out.println("Previous Favorite Song: " + favoriteSong);
-    favoriteSong = newSong;
-    System.out.println("New Favorite Song: " + favoriteSong);
-    System.out.println("==============");
-    System.out.println(" ");
+
+    public void performForAudience(singer_exercise2 singer, int people) {
+        noOfPerformances++;
+        singer.noOfPerformances++;
+        totalPerformances++;  // add by 1 or 2??
+        singer.earnings += people * 50;
+        earnings += people * 50;
+    }
+
+    public void changeFavSong(singer_exercise2 song) {
+        favoriteSong = song;
+    }
+
+    public double getEarnings() {
+        return earnings;
+    }
+
+    public singer_exercise2 getFavoriteSong() {
+        return favoriteSong;
+    }
+
+    public static int getTotalPerformances() {
+        return totalPerformances;
     }
 }
