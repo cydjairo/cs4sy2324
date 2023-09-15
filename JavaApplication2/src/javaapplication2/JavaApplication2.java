76caf38b5aeb60d02bbbb28a342d939301c305c5
cydjairo <112676628+cydjairo@users.noreply.github.com>
2023-09-15 22:51:51 +0800

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,9 +15,9 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         
-        song_exercise2 First = new song_exercise2(1, "WAR-R-R", "Love Part 1", "4 mins, 32 secs", "Colde");
-        song_exercise2 Second = new song_exercise2(2, "ONLY", "4 ONLY", "4 mins.", "LeeHi");
-        song_exercise2 Third = new song_exercise2 (3, "bad", "0.1 flaws and all.", "4 mins, 23 secs", "wave to earth");
+        song_exercise3 First = new song_exercise3(1, "WAR-R-R", "Love Part 1", "4 mins, 32 secs", "Colde");
+        song_exercise3 Second = new song_exercise3(2, "ONLY", "4 ONLY", "4 mins.", "LeeHi");
+        song_exercise3 Third = new song_exercise3 (3, "bad", "0.1 flaws and all.", "4 mins, 23 secs", "wave to earth");
         
         singer_exercise2 Anne = new singer_exercise2("Anne Ambrocio", 10, 5000, "Paraluman");
         
@@ -25,7 +25,8 @@ public class JavaApplication2 {
         Second.displaySonging();
         Third.displaySonging();
          
-        Anne.performForTheAudience(100);
+        Anne.performForTheAudience(40000);
+        Anne.changeFavoriteSong("Uhaw");
         
         valorant_exercise2 Jett = new valorant_exercise2(10, "Jett", "South Korea", "Duelist", "Cloudburst", 200, "Updraft", 150, "Tailwind", "Blade Storm", 8, "Can't Slow Me Down");
         valorant_exercise2 Reyna = new valorant_exercise2(11, "Reyna", "Mexico", "Duelist", "Leer", 250, "Devour", 200, "Dismiss", "Empress", 6, "Tell Me You'll Never Leave");
